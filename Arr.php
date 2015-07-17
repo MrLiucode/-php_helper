226dfcde09f8e154ccb498bc81913adedda571d5
lilichun <lilichun@anshengcredit.com>
2015-07-17 14:19:41 +0800

    数组函数

---
+++ Arr.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Arr.php
+ *
+ * Part of Allinpay.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author    Fackeronline <1077341744@qq.com>
+ * @link      https://github.com/Fakeronline
+ */
+
+/**
+ * 从数组中获取某个值，可深度获取
+ * @param $array
+ * @param $key
+ * @param null $default
+ * @return null
+ */
+function get($array, $key, $default = null)
+{
+    if (is_null($key)) {
+        return $array;
+    }
+
+    if (isset($array[$key])) {
+        return $array[$key];
+    }
+
+    foreach (explode('.', $key) as $segment) {
+        if (!is_array($array) || !array_key_exists($segment, $array)) {
+            return $default;
+        }
+        $array = $array[$segment];
+    }
+
+    return $array;
+}
